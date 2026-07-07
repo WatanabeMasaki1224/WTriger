dba0033b6aa0a1ef2f0d2af08f5c9b5deeb3a446
渡邉雅紀 <masa101224@outlook.jp>
2026-07-07 15:47:59 +0900

    エネミーのHPバーの色を体力に応じて変更するように設定

--- Assets/Script/Enemy/EnemyHP.cs
+++ Assets/Script/Enemy/EnemyHP.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,9 +6,30 @@ public class EnemyHP : MonoBehaviour
 {
     [SerializeField] private Image _hpBar;
     [SerializeField] private EnemyBase _enemy;
+    [SerializeField] private Color _normalColor = Color.green;
+    [SerializeField] private Color _phase2Color = Color.yellow;
+    [SerializeField] private Color _lastColor = Color.red;
 
     private void Update()
     {
-        _hpBar.fillAmount = _enemy.HP / _enemy.MaxHP;
+        float hpRate = _enemy.HP / _enemy.MaxHP;
+        _hpBar.fillAmount = hpRate;
+        UpdateColor(hpRate);
+    }
+
+    void UpdateColor(float rate)
+    {
+        if (rate <= 0.25f)
+        {
+            _hpBar.color = _lastColor;
+        }
+        else if (rate <= 0.5f)
+        {
+            _hpBar.color = _phase2Color;
+        }
+        else
+        {
+            _hpBar.color = _normalColor;
+        }
     }
 }
